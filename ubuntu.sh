270df0012808941d7d7b8c928e75bec51af2700b
HDVinnie <hdinnovations@protonmail.com>
2024-01-10 18:41:39 -0500

    update: ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -47,7 +47,7 @@ add_pkgs() {
     # PHP
     echo -e "\n$Cyan Installing PHP ... $Color_Off"
 
-    apt-get install -qq curl debconf-utils php-pear php8.2-curl php8.2-dev php8.2-gd php8.2-mbstring php8.2-zip php8.2-mysql php8.2-xml php8.2-fpm php8.2-intl php8.2-bcmath php8.2-redis > /dev/null
+    apt-get install -qq curl php-pear php8.3-common php8.3-cli php8.3-fpm php8.3-{redis,bcmath,curl,dev,gd,igbinary,intl,mbstring,mysql,opcache,readline,xml,zip}
     check $? "Installing PHP Failed!"
 
     echo -e "$IGreen OK $Color_Off"
