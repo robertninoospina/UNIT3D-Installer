321ab350f25d6f4dbdd73b8314196bc943d0ab87
robertninoospina <166242738+robertninoospina@users.noreply.github.com>
2024-10-12 10:14:07 -0500

    Update app.php

--- src/Configs/app.php
+++ src/Configs/app.php
@@ -5,7 +5,7 @@ use App\Installer\Database\MySqlSetup;
 return [
     'min_php_version' => '8.2',
 
-    'repository' => 'https://github.com/robertninoospina/UNIT3D-Community-Edition.git',
+    'repository' => 'https://github.com/robertninoospina/UNIT3D-Community-Edition-8.3.1.git',
 
     'database_installers' => [
         /**
